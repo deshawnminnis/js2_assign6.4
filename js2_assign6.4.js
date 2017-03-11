56fec6250f3b1a2acdd1f29871bbb1e02eb8b132
deshawnminnis <deshawnminnis@gmail.com>
2017-03-10 21:42:21 -0800

    Add files via upload

---
+++ js2_assign6.4.js
@@ -0,0 +1,8 @@
+function checkingFor() {
+    var employee = ["De'Shawn Minnis", "37", "1500.00", "18025 272nd St",
+        "Covington", "WA", "98042"
+    ];
+    for (var i = 0; i < employee.length; i++) {
+        console.log(employee[i]);
+    }
+}
